d9722917b1ee6c1ab3bc019ce4078cd77a53074f
Aaron Smith <smithaaron654@gmail.com>
2023-04-18 14:46:36 -0400

    changes made to script

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a personal project I made that displays the date and time for the user. 
+
+[Live Demo](https://aar654.github.io/Clock/) ⏰
--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+//query selectors to get HTML selectors
 const date = document.querySelector("#date");
 const hr = document.querySelector("#hr");
 const min = document.querySelector("#min");
 const sec = document.querySelector("#sec");
+const amPM = document.querySelector("#amPM")
 
 let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-let today  = new Date();
 
-date.innerHTML = today.toLocaleDateString("en-US");
-hr.innerHTML = today.getHours() + ":";
-min.innerHTML = today.getMinutes() + ":";
-sec.innerHTML = today.getSeconds() + "";
+//function that updates the clock and calender every second
+function clockUpdater(){
+    let today = new Date();
+
+    date.innerHTML = today.toLocaleDateString("en-US");
+    
+    if (today.getSeconds() < 10) {
+        sec.innerHTML = "0" + today.getSeconds() + "";
+    } else{
+        sec.innerHTML = today.getSeconds() + "";
+    }    
+
+    if (today.getHours() > 12) {
+        hr.innerHTML = today.getHours() - 12 + ":";
+        amPM.innerHTML = "PM"
+    } else {
+        amPM.innerHTML = "AM"
+    }
+
+    if (today.getMinutes() < 10) {
+        min.innerHTML = "0" + today.getMinutes() + ":";
+    } else {
+        min.innerHTML = today.getMinutes() + ":";
+    }
+}
+
+setInterval(clockUpdater,1000)
+
+
+
 
 
 
